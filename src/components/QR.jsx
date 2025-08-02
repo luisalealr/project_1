2097cbd36f0ae8799271c78b38fab25388e204bc
luisalealr <luisafernandalero@gmail.com>
2025-08-01 20:02:55 -0500

    Change qr

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -3,7 +3,11 @@ import style from "./QR.module.css";
 const QR = () => {
   return (
     <div className={style.qr_container}>
-      <img className={style.qr_image} src="../../public/image-qr.png" alt="" />
+      <img
+        className={style.qr_image}
+        src="https://icon-library.com/images/qr-code-icon/qr-code-icon-18.jpg"
+        alt=""
+      />
     </div>
   );
 };
